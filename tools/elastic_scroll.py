9432f6dbc2ae7b0b05d0e514bebac2a7deb55aaa
Javier Fuentes <0xffaa.rm@gmail.com>
2016-01-27 12:19:09 -0300

    updates

--- tools/elastic_scroll.py
+++ tools/elastic_scroll.py
@@ -2,27 +2,34 @@
 import urllib.request as urllib
 import json
 
-
+url_base = 'http://elk-master.osf.alma.cl:9200'
 headers = {}
 headers['Content-Type'] = 'application/json'
+results = list()
+
+def scroll():
+    """
+    Obtiene el primer resultado incluyendo el Scroll_id para continuar obteniendo los datos.
+    """
+    url1 = url_base + '/aos-*/_search?q=LogLevel:Critical&scroll=1m'
+
+    data1 = {"fields" : ["TimeStamp", "text"]}
+    data1 = json.dumps(data1).encode('utf-8')
+
+    req1 = urllib.Request(url1, data1, headers)
+    res1 = urllib.urlopen(req1)
+
+    res1 = res1.read().decode('utf-8')
+    res1 = json.loads(res1)
+    scroll_id = res1["_scroll_id"]
+    return (scroll_id, res1['hits']['hits'])
 
 
-url1 = 'http://elk-master.osf.alma.cl:9200/aos-*/_search?q=LogLevel:Critical&scroll=1m'
-
-data1 = '{"fields" : ["TimeStamp", "text"]}'
-data1 = data1.encode('utf-8')
-
-req1 = urllib.Request(url1, data1, headers)
-res1 = urllib.urlopen(req1)
-
-res1 = res1.read().decode('utf-8')
-res1 = json.loads(res1)
-
-
-scroll_id = res1["_scroll_id"]
-
-def reescroll():
-    url2 = 'http://elk-master.osf.alma.cl:9200/_search/scroll'
+def reescroll(scroll_id):
+    """
+    Reutiliza el scroll_id para continuar obteniendo los datos.
+    """
+    url2 = url_base + '/_search/scroll'
     data2 = {"scroll": "1m", "scroll_id": scroll_id}
     data2 = json.dumps(data2).encode('utf-8')
 
@@ -31,3 +38,19 @@ def reescroll():
 
     res2 = res2.read().decode('utf-8')
     res2 = json.loads(res2)
+
+    return res2['hits']['hits']
+
+
+def main():
+    scroll_id, results = scroll()
+    aux = reescroll(scroll_id)
+    i=0
+    while len(aux) != 0:
+        print ("Reescrolling" + str(i))
+        i+=1
+        results = results + aux
+        aux = reescroll(scroll_id)
+
+if __name__ == '__main__':
+    main()
